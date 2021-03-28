04a18d9126ac6e2a56929a71929dd1813a6ed182
Xoom7573 <66717268+Xoom7573@users.noreply.github.com>
2021-03-28 11:22:54 +0200

    Update
    Some Examples i found online on articles etc...
    Doing some tests with it.

--- src/network_2.js
+++ src/network_2.js
@@ -2,14 +2,16 @@ const brain = require('brain.js');
 
 const network = new brain.NeuralNetwork();
 
+/*
+We have football teams playing each other and the ai 
+decides who will more likely win between team 1 and 4. 
+*/
+
 network.train([
   { input: [1, 2], output: [1] },   // Team 2 wins
   { input: [1, 3], output: [1] },   // Team 3 wins
   { input: [2, 3], output: [0] },   // Team 2 wins
-  { input: [2, 4], output: [1] },   // Team 4 wins
-  { input: [1, 2], output: [0] },   // Team 1 wins
-  { input: [1, 3], output: [0] },   // Team 1 wins
-  { input: [3, 4], output: [0] }    // Team 3 wins
+  { input: [2, 4], output: [1] }    // Team 4 wins
 ]);
 
 const output = network.run([1, 4]); // team 4 should be winning!
